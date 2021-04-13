dfe609cffc617996bc3db9f8becc3d895e9effae
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-04-13 17:07:17 +0200

    ajout quote card + css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
 import React from 'react'
-import Quote from './components/Quote'
+import Quote from './components/QuoteCard.js'
 import './App.css';
+import axios from 'axios'
+
+
+
 
 function App() {
+
+  const [quote, SetQuote] = useState('')
+
+  const getQuote = () =>{
+
+      axios.get(`https://simpsons-quotes-api.herokuapp.com/quotes`)
+      .then((response) => response.data)
+      .then((data) => {
+          console.log([data.results]);
+          }) }
+
   return (
     <div className="App">
-
+        <quoteCard 
+        quote={quote.quote}
+        character={quote.character} />
+        <button>Get a new quote</button>
     </div>
   );
 }
